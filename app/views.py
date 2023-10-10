c082c6d6d609fc1b7bbdbdb6e60b6967d12fb4d3
swesadiqul <mdsadiqulislam390@gmail.com>
2023-10-11 00:25:41 +0600

    change model form with different activation

--- app/views.py
+++ app/views.py
@@ -1,28 +1,45 @@
 from django.shortcuts import render, redirect
-from .forms import BlogForm, BlogImageForm
-from .models import Blog, BlogImage
+from .forms import *
+from django.forms import modelformset_factory
+from .models import *
+
+
+
 
 def create_blog(request):
+    categories = Category.objects.all()
+    extra_forms = int(request.GET.get('extra_forms', 1))
+    
+    # AuthorFormSet = forms.modelformset_factory(Author, form=AuthorForm, extra=extra_forms, max_num=extra_forms)
+    BlogImageFormSet = modelformset_factory(BlogImage, form=BlogImageForm, extra=extra_forms, max_num=extra_forms)
     if request.method == 'POST':
-        form = BlogForm(request.POST, request.FILES)
-        if form.is_valid():
-            # Create the blog post
-            blog = form.save(commit=False)
-            # blog.author = request.user  # Assuming you have user authentication
+        blog_form = BlogForm(request.POST)
+        image_formset = BlogImageFormSet(request.POST, request.FILES, queryset=BlogImage.objects.none())
+
+        if blog_form.is_valid() and image_formset.is_valid():
+            # Save the blog post
+            blog = blog_form.save(commit=False)
+            blog.author = request.user  # Assuming you have user authentication
             blog.save()
 
-            # Handle the BlogImage formset
-            BlogImageFormSet = form.formset_factory(BlogImageForm, extra=1)
-            formset = BlogImageFormSet(request.POST, request.FILES, prefix='blog_images')
-            if formset.is_valid():
-                for img_form in formset:
-                    if img_form.cleaned_data:
-                        image = img_form.cleaned_data['image']
-                        BlogImage.objects.create(blog_post=blog, image=image)
+            # Associate images with the blog post
+            for img_form in image_formset:
+                if img_form.cleaned_data:
+                    image = img_form.cleaned_data['image']
+                    BlogImage.objects.create(blog_post=blog, image=image)
 
-            return redirect('/')  # Redirect to the blog post detail page
+            return redirect('/')  # Redirect to the blog post detail page or another appropriate page
+
     else:
-        form = BlogForm()
+        blog_form = BlogForm()
+        image_formset = BlogImageFormSet(queryset=BlogImage.objects.none())
 
-    return render(request, 'index.html', {'form': form})
+    
+    context = {
+        'blog_form': blog_form, 
+        'image_formset': image_formset, 
+        'extra_forms': extra_forms,
+        'categories': categories,
+        }
 
+    return render(request, 'index.html', context)
--- formset/settings.py
+++ formset/settings.py
@@ -26,6 +26,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app',
+    "crispy_forms",
+    "crispy_bootstrap5",
 ]
 
 MIDDLEWARE = [
@@ -110,3 +112,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
